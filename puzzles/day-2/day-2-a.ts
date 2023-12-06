59be108c9842f9c9bfc426ea5730c30507fa9ebd
lin.yu <lin.yu@tigergraph.com>
2023-12-06 18:05:44 +0800

    day2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,53 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function getNumber(s: string): number {
+  let [s1, s2] = s.split(':');
+  let gameNumber = Number(s1.slice('game'.length));
+
+  let cubeLists = s2.split(';');
+  for (let cubeList of cubeLists) {
+    let subCubs = cubeList.split(',');
+    for (let subCub of subCubs) {
+      let [s1, s2] = subCub.trim().split(' ');
+      let number = Number(s1);
+      s2 = s2.trim();
+      switch (s2) {
+        case 'red':
+          if (number > 12) {
+            return 0;
+          }
+          break;
+
+        case 'green':
+          if (number > 13) {
+            return 0;
+          }
+          break;
+
+        case 'blue':
+          if (number > 14) {
+            return 0;
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  return gameNumber;
+}
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+
+  for (let s of data) {
+    sum += getNumber(s);
+  }
+  return sum;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,54 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function getNumber(s: string): number {
+  let [_, s2] = s.split(':');
+  let cubeLists = s2.split(';');
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+  for (let cubeList of cubeLists) {
+    let subCubs = cubeList.split(',');
+    for (let subCub of subCubs) {
+      let [s1, s2] = subCub.trim().split(' ');
+      let number = Number(s1);
+      s2 = s2.trim();
+      switch (s2) {
+        case 'red':
+          if (number > red) {
+            red = number;
+          }
+          break;
+
+        case 'green':
+          if (number > green) {
+            green = number;
+          }
+          break;
+
+        case 'blue':
+          if (number > blue) {
+            blue = number;
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  return red * green * blue;
+}
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+
+  for (let s of data) {
+    sum += getNumber(s);
+  }
+  return sum;
 }
 
 const answer = await day2b();
