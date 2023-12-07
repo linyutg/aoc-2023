a6ec32bd493119a4dd11b6b692eb9792eb23d227
lin.yu <lin.yu@tigergraph.com>
2023-12-07 16:32:21 +0800

    day6

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -3,7 +3,30 @@ import chalk from 'chalk';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const times = data[0].split(':')[1].trim().split(/\s+/).map(Number);
+  const distances = data[1].split(':')[1].trim().split(/\s+/).map(Number);
+
+  const counts: number[] = new Array(times.length).fill(0);
+
+  for (let i = 0; i < times.length; i++) {
+    const time = times[i];
+
+    for (let j = 0; j <= time; j++) {
+      const distance = (time - j) * j;
+      if (distance > distances[i]) {
+        counts[i]++;
+      }
+    }
+  }
+
+  console.log(counts);
+  let sum = 1;
+  for (let count of counts) {
+    sum = sum * count;
+  }
+
+  return sum;
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -3,7 +3,20 @@ import chalk from 'chalk';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const time = Number(data[0].split(':')[1].trim().split(/\s+/).join(''));
+  const distance = Number(data[1].split(':')[1].trim().split(/\s+/).join(''));
+
+  let count = 0;
+
+  for (let j = 0; j <= time; j++) {
+    const dis = (time - j) * j;
+    if (dis > distance) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 const answer = await day6b();
