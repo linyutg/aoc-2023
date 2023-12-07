4e7a9e15947270873395e990a63c3450525187a4
lin.yu <lin.yu@tigergraph.com>
2023-12-07 16:19:30 +0800

    day 5

--- puzzles/day-5/day-5-b.ts
+++ puzzles/day-5/day-5-b.ts
@@ -1,9 +1,97 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Range = {
+  destStart: number;
+  sourceStart: number;
+  length: number;
+};
+
+function inRange(range: Range, source: number): boolean {
+  if (
+    source >= range.sourceStart &&
+    source < range.sourceStart + range.length
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function convertToRange(range: Range, source: number): number {
+  return range.destStart + source - range.sourceStart;
+}
+
+function parseRanges(rows: string[]): Range[][] {
+  let ret: Range[][] = [];
+
+  let range: Range[] = [];
+  for (let row of rows) {
+    if (row === '') {
+      if (range.length > 0) {
+        ret.push(range);
+      }
+      range = [];
+      continue;
+    }
+    if (row.includes(':')) {
+      continue;
+    }
+    const [destStart, sourceStart, length] = row.trim().split(' ').map(Number);
+    range.push({
+      destStart,
+      sourceStart,
+      length,
+    });
+  }
+
+  if (range.length > 0) {
+    ret.push(range);
+  }
+
+  return ret;
+}
+
+function parseSeeds(row: string): number[] {
+  const [_, s2] = row.split(':');
+  const ret = s2.trim().split(' ').map(Number);
+  return ret;
+}
+
+function map(ranges: Range[][], seed: number): number {
+  let start = seed;
+  for (let rangeList of ranges) {
+    for (let range of rangeList) {
+      if (inRange(range, start)) {
+        start = convertToRange(range, start);
+        break;
+      }
+    }
+  }
+  return start;
+}
+
 export async function day5b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const seeds = parseSeeds(data[0]);
+  const ranges = parseRanges(data.slice(1));
+
+  let min = 0;
+
+  for (let i = 0; i < seeds.length; i += 2) {
+    const start = seeds[i];
+    const end = seeds[i + 1];
+    console.log(start, end);
+
+    for (let j = 0; j < end; j++) {
+      const location = map(ranges, start + j);
+      if (location < min || min === 0) {
+        min = location;
+      }
+    }
+  }
+
+  return min;
 }
 
 const answer = await day5b();
