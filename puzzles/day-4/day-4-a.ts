594a59b3b9ed281e496e7be27224c3eeb053ebf2
lin.yu <lin.yu@tigergraph.com>
2023-12-07 09:28:38 +0800

    day4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,9 +1,36 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function getNumber(row: string): number {
+  let [_, s] = row.split(':');
+  let [s1, s2] = s.split('|');
+
+  let list1 = s1.trim().split(/\s+/);
+  let list2 = s2.trim().split(/\s+/);
+
+  let num = 0;
+
+  for (let item of list2) {
+    if (list1.includes(item)) {
+      num++;
+    }
+  }
+
+  if (num == 0) {
+    return 0;
+  }
+
+  return Math.pow(2, num - 1);
+}
+
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+
+  for (let s of data) {
+    sum += getNumber(s);
+  }
+  return sum;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,46 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function processRow(
+  row: string,
+  index: number,
+  scratchcards: number[],
+  multi: number
+) {
+  let [_, s] = row.split(':');
+  let [s1, s2] = s.split('|');
+
+  let list1 = s1.trim().split(/\s+/);
+  let list2 = s2.trim().split(/\s+/);
+
+  let num = 0;
+
+  for (let item of list2) {
+    if (list1.includes(item)) {
+      num++;
+    }
+  }
+
+  for (let i = 1; i <= num; i++) {
+    scratchcards[index + i] += multi;
+  }
+}
+
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let scratchcards = new Array<number>(data.length).fill(1);
+
+  for (let i = 0; i < data.length; i++) {
+    const row = data[i];
+    processRow(row, i, scratchcards, scratchcards[i]);
+  }
+
+  let sum = 0;
+  for (let s of scratchcards) {
+    sum += s;
+  }
+  return sum;
 }
 
 const answer = await day4b();
