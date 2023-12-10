0c19f855ecd7914e05e1c0c70ce3e2bc22543ac1
lin.yu <lin.yu@tigergraph.com>
2023-12-10 11:19:38 +0800

    day9

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -1,9 +1,43 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function diffSequence(numbers: number[]): number {
+  let current = numbers;
+  let ret = numbers[numbers.length - 1];
+  while (true) {
+    let array: number[] = [];
+
+    for (let i = 1; i < current.length; i++) {
+      const diff = current[i] - current[i - 1];
+      array.push(diff);
+    }
+    ret += array[array.length - 1];
+
+    let flag = true;
+    for (let v of array) {
+      if (v != 0) {
+        flag = false;
+        break;
+      }
+    }
+
+    if (flag) {
+      return ret;
+    }
+    current = array;
+  }
+}
+
 export async function day9a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let sum = 0;
+  for (let s of data) {
+    const numbers = s.split(' ').map(Number);
+    sum += diffSequence(numbers);
+  }
+
+  return sum;
 }
 
 const answer = await day9a();
--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -1,9 +1,51 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function diffSequence(numbers: number[]): number {
+  let current = numbers;
+  let seqs = [numbers[0]];
+  while (true) {
+    let array: number[] = [];
+
+    for (let i = 1; i < current.length; i++) {
+      const diff = current[i] - current[i - 1];
+      array.push(diff);
+    }
+
+    seqs.push(array[0]);
+    let flag = true;
+    for (let v of array) {
+      if (v != 0) {
+        flag = false;
+        break;
+      }
+    }
+
+    if (flag) {
+      break;
+    }
+    current = array;
+  }
+
+  let ret = 0;
+  let add = true;
+  for (let seq of seqs) {
+    ret += add ? seq : -seq;
+    add = !add;
+  }
+  return ret;
+}
+
 export async function day9b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let sum = 0;
+  for (let s of data) {
+    const numbers = s.split(' ').map(Number);
+    sum += diffSequence(numbers);
+  }
+
+  return sum;
 }
 
 const answer = await day9b();
